9f85c4735f42a8475f5994c263f78e7df2fde8e5
Lilach Naor <55951495+Lilach26@users.noreply.github.com>
2021-07-04 00:01:31 +0300

    Update Program.java

--- Stack/Program.java
+++ Stack/Program.java
@@ -2,7 +2,7 @@ public class Program
 {
     public static void main(String[] args)
     {
-        Stack stack = new Stack();
+        Stack<Integer> stack = new Stack<>();
         stack.push(1);
         stack.push(2);
         stack.push(3);
